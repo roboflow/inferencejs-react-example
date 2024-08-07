fdbe00bc4e7a8b7397ac6cd4cc22699c82c4f5a2
Maxwell Stone <38924020+MadeWithStone@users.noreply.github.com>
2024-08-07 15:42:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { InferenceEngine, CVImage } from "inferencejs";
+import { useEffect, useRef, useState, useMemo } from "react";
 
 function App() {
+  const inferEngine = useMemo(() => {
+    return new InferenceEngine();
+  }, []);
+  const [modelWorkerId, setModelWorkerId] = useState(null);
+  const [modelLoading, setModelLoading] = useState(false);
+
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    if (!modelLoading) {
+      setModelLoading(true);
+      inferEngine
+        .startWorker(
+          "hard-hat-sample-txcpu",
+          3,
+          "rf_EsVTlbAbaZPLmAFuQwWoJgFpMU82"
+        )
+        .then((id) => setModelWorkerId(id));
+    }
+  }, [inferEngine, modelLoading]);
+
+  useEffect(() => {
+    console.log("Model Worker ID: " + modelWorkerId);
+    if (modelWorkerId) {
+      startWebcam();
+    }
+  }, [modelWorkerId]);
+
+  const startWebcam = () => {
+    var constraints = {
+      audio: false,
+      video: {
+        width: { ideal: 640 },
+        height: { ideal: 480 },
+        facingMode: "environment",
+      },
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      videoRef.current.srcObject = stream;
+      videoRef.current.onloadedmetadata = function () {
+        videoRef.current.play();
+      };
+
+      videoRef.current.onplay = () => {
+        var ctx = canvasRef.current.getContext("2d");
+
+        var height = videoRef.current.videoHeight;
+        var width = videoRef.current.videoWidth;
+
+        videoRef.current.width = width;
+        videoRef.current.height = height;
+
+        canvasRef.current.width = width;
+        canvasRef.current.height = height;
+
+        ctx.scale(1, 1);
+
+        detectFrame();
+      };
+    });
+  };
+
+  const detectFrame = () => {
+    if (!modelWorkerId) setTimeout(detectFrame, 100 / 3);
+
+    const img = new CVImage(videoRef.current);
+    inferEngine.infer(modelWorkerId, img).then((predictions) => {
+      var ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+      for (var i = 0; i < predictions.length; i++) {
+        console.log(predictions[i]);
+        var prediction = predictions[i];
+
+        // draw detections
+        ctx.strokeStyle = prediction.color;
+
+        var x = prediction.bbox.x - prediction.bbox.width / 2;
+        var y = prediction.bbox.y - prediction.bbox.height / 2;
+        var width = prediction.bbox.width;
+        var height = prediction.bbox.height;
+
+        ctx.rect(x, y, width, height);
+        ctx.fillStyle = "rgba(0, 0, 0, 0)";
+        ctx.fill();
+        ctx.fillStyle = ctx.strokeStyle;
+        ctx.lineWidth = "4";
+        ctx.strokeRect(x, y, width, height);
+
+        var text = ctx.measureText(
+          prediction.class + " " + Math.round(prediction.confidence * 100) + "%"
+        );
+        ctx.fillStyle = ctx.strokeStyle;
+        ctx.fillRect(x - 2, y - 30, text.width + 4, 30);
+        ctx.font = "15px monospace";
+        ctx.fillStyle = "black";
+        ctx.fillText(
+          prediction.class +
+            " " +
+            Math.round(prediction.confidence * 100) +
+            "%",
+          x,
+          y - 10
+        );
+      }
+
+      setTimeout(detectFrame, 100 / 3);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ position: "relative" }}>
+        <video
+          id="video"
+          width="640"
+          height="480"
+          ref={videoRef}
+          style={{ position: "relative" }}
+        />
+        <canvas
+          id="canvas"
+          width="640"
+          height="480"
+          ref={canvasRef}
+          style={{ position: "absolute", top: 0, left: 0 }}
+        />
+      </div>
     </div>
   );
 }
